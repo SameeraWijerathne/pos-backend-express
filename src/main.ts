175a97f30544d45536317712548654de296c909f
Sameera <sameerawncd@gmail.com>
2023-07-29 14:03:12 +0530

    Set up routing for customer-router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
+import {router as customerRouter} from "./api/customer-router";
 
 const app = express();
-
+app.use('/pos/api/v1/customer', customerRouter);
 app.listen(8080, () => console.log("Server has been started at 8080"));
