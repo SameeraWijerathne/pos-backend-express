6be61072b789af8937a3849cff3126e136574d6d
Sameera <sameerawncd@gmail.com>
2023-07-29 14:49:32 +0530

    Set up middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import express, {json} from 'express';
 import {router as customerRouter} from "./api/customer-router";
+import cors from 'cors';
 
 const app = express();
+app.use(json());
+app.use(cors());
 app.use('/pos/api/v1/customer', customerRouter);
 app.listen(8080, () => console.log("Server has been started at 8080"));
